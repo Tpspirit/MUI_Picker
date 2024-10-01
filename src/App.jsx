21d85d8959622ef2a96db50128f7de78f26c9e6f
Phan Khoi <giakhoi2015@gmail.com>
2024-10-01 10:06:57 +0300

    Add AppBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import "./App.css";
 import TodoList from "./TodoList";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
-  return <TodoList />;
+  return (
+    <Container maxWidth="xl">
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">My Todos</Typography>
+        </Toolbar>
+      </AppBar>
+      <TodoList />
+    </Container>
+  );
 }
 
 export default App;
